a76f1317113e896c94f40d102f1b79092f706787
PythonDeveloper06 <118295084+PythonDeveloper06@users.noreply.github.com>
2026-01-08 23:47:58 +0600

    Update settings.py

--- SeldWeb/settings.py
+++ SeldWeb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0vvn8b7ejc2%u89*_2r69jwp9ra47h-gjd%#i$gysuxf%qbb7o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['seld-lock.ru', '127.0.0.1', 'seldweb-production.up.railway.app']
 
